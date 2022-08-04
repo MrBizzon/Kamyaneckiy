6a581c5ad8f44b21449551a0a1c5ee7d64146a41
MrBizzon <oleg.zubarev@mail.ru>
2022-08-04 13:03:15 +0300

    add 5 file

---
+++ Example005_New/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Дура пришла");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
